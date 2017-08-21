67864aa7f5168139fae1c0d0fe062359fedafabc
sreeram177 <sreeram177@gmail.com>
2017-08-21 17:17:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article1',function(req,res){
    res.send("Article1 is requested. Will be served shortly");
 });
 
+app.get('/article2',function(req,res){
+   res.send("Article2 is requested. Will be served shortly");
+});
+
+app.get('/article2',function(req,res){
+   res.send("Article2 is requested. Will be served shortly");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
